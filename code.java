b35571b15894ee319c0c5331502c32df02c9c039
GSU Student <Students07@IIT-DC-L091.dmd.gsuad.gsu.edu>
2021-08-01 19:31:14 -0400

    Styling document about HealthMaster

---
+++ code.java
@@ -0,0 +1,8 @@
+public class code {
+    public static void main(String[] args) {
+        String name = "Aysha";
+        System.out.println("Good Morning, " + name);
+        System.out.println("You are the dumbest person I know");
+    }
+    
+}
